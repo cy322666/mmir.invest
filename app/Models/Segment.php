d3bc2cc91cc8972196a9d01597bb87ab295a175a
cy322666 <cy322666@gmail.com>
2022-12-12 17:34:24 +0200

    build segment flow

--- app/Models/Segment.php
+++ app/Models/Segment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Segment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'lead_id',
+        'contact_id',
+        'sale',
+        'sale_invest',
+        'sale_apart',
+        'count_leads',
+        'is_double',
+        'link_double_phone',
+        'link_double_email',
+        'count_leads_invest',
+        'count_leads_apart',
+    ];
 }
--- database/migrations/2022_12_12_080726_create_segments_table.php
+++ database/migrations/2022_12_12_080726_create_segments_table.php
@@ -16,6 +16,18 @@ return new class extends Migration
         Schema::create('segments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->integer('lead_id')->nullable();
+            $table->integer('contact_id')->nullable();
+            $table->integer('sale')->default(0);
+            $table->integer('sale_invest')->default(0);
+            $table->integer('sale_apart')->default(0);
+            $table->integer('count_leads')->default(1);
+            $table->boolean('is_double')->default(false);
+            $table->string('link_double_phone')->nullable();
+            $table->string('link_double_email')->nullable();
+            $table->integer('count_leads_invest')->default(0);
+            $table->integer('count_leads_apart')->default(0);
         });
     }
 
